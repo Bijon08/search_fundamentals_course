c65fe1a24ce3976493003bc52e6685a52908c38a
Bijon08 <bijonkumarbose90@gmail.com>
2023-02-20 05:30:53 +0000

    Add autocomplete and spell check

--- week2/search.py
+++ week2/search.py
@@ -64,7 +64,23 @@ def autocomplete():
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
+            suggester = {
+                "suggest":{
+                    "autocomplete":{
+                        "prefix": prefix,
+                        "completion":{
+                            "field": "suggest",
+                            "skip_duplicates": True
+                        }
+                    }
+                },
+                "_source": "canonical"
+            }
+            if type == "queries":
+                index_name = "bbuy_queries"
+            elif type == "products":
+                index_name = "bbuy_products"
+            search_response = get_opensearch().search(body=suggester, index=index_name)
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
@@ -108,6 +124,7 @@ def query():
         ##### W2, L1, S2
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -123,7 +140,7 @@ def query():
         #### W2, L1, S2
 
         ##### W2, L2, S2
-
+        qu.add_spelling_suggestions(query_obj, user_query)
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
